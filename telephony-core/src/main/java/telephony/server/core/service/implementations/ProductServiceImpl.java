44a360cf1f3085dcb5c3535382ef9862a0e81859
Pawel Henek <pawelhenek@gmail.com>
2012-10-09 10:58:33 +0200

    Divided project into 2 modules (maven and gwt) : core and gwt

---
+++ telephony-core/src/main/java/telephony/server/core/service/implementations/ProductServiceImpl.java
@@ -0,0 +1,194 @@
+package telephony.server.core.service.implementations;
+
+
+import com.google.inject.Inject;
+import com.google.inject.persist.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import telephony.server.core.dao.interfaces.ProductsDao;
+import telephony.server.core.entity.Product;
+import telephony.server.core.entity.Store;
+import telephony.server.core.entity.User;
+import telephony.server.core.entity.common.Money;
+import telephony.server.core.entity.common.ProductStatus;
+import telephony.server.core.service.interfaces.ProductService;
+
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class ProductServiceImpl implements ProductService {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Inject
+    private EntityManager em;
+
+    @Inject
+    private ProductsDao productsDao;
+
+    public List<String> fetchAllImeiInUse() {
+        logger.debug("ProductServiceImpl.fetchAllImeiInUse starts");
+        
+        List<String> res = productsDao.fetchImeisList();
+        
+        logger.debug("found {} elements", res.size());
+        
+        return res;
+    }
+
+    public List<String> fetchAllProducers() {
+        logger.debug("ProductServiceImpl.fetchAllProducers starts");
+        
+        List<String> res = new ArrayList<String>();
+        List<Product> products = productsDao.findUndeleted();
+
+        for (Product p : products) {
+            if (!res.contains(p.getProducer()))
+                res.add(p.getProducer());
+        }
+        
+        logger.debug("found {} elements " , res.size());
+        
+        return res;
+    }
+
+    public List<String> fetchAllModels() {
+        logger.debug("ProductServiceImpl.fetchAllModels starts");
+
+        List<String> res = new ArrayList<String>();
+        List<Product> products = productsDao.findUndeleted();
+
+        for (Product p : products) {
+            if (!res.contains(p.getModel()))
+                res.add(p.getModel());
+        }
+
+        logger.debug("found {} elements " , res.size());
+
+        return res;
+    }
+
+    public List<String> fetchAllColors() {
+        logger.debug("ProductServiceImpl.fetchAllModels starts");
+
+        List<String> res = new ArrayList<String>();
+        List<Product> products = productsDao.findUndeleted();
+
+        for (Product p : products) {
+            if (!res.contains(p.getColor()))
+                res.add(p.getColor());
+        }
+
+        logger.debug("found {} elements " , res.size());
+
+        return res;
+    }
+
+
+    public List<Product> fetchAllProducts(Long storeId, ProductStatus productStatus) {
+
+        logger.debug("ProductServiceImpl.fetchAllProducts starts ");
+        logger.debug("params : [ storeId : {} , productStatus : {} ] ", storeId, productStatus);
+
+        List<Product> result = productsDao.findByStoreId(storeId, productStatus);
+
+        logger.debug("ProductServiceImpl.fetchAllProducts ends");
+
+        return  result;
+    }
+
+    public void moveProducts(Store store, List<Product> products, User user) {        
+        logger.debug("ProductServiceImpl.moveProducts starts ");
+        logger.debug("params : [ storeId : {} , number of products: {} , userId : {}] ", new Object[] {store, products, user});
+        
+        em.getTransaction().begin();
+        
+        for (Product p : products) {
+            p.setStore(store);
+            p.setEditor(user);
+            p.setEditedAt(new Date());
+
+            productsDao.save(p);
+        }
+        
+        em.getTransaction().commit();
+        
+        logger.debug("ProductServiceImpl.fetchAllProducts ends");        
+    }
+
+    public Product fetchProductByImeiAndStoreId(String imei, Long storeId) {
+        logger.debug("ProductServiceImpl.fetchProductByImeiAndStoreId starts");
+        
+        Product p = productsDao.findByImeiAndStoreId(imei, storeId);
+        
+        return p;
+    }
+
+    @Transactional
+    public List<Product> fetchAllProductsByCriteria(String imei, String producer, String model, String color, Long storeId, Date deliveryDateStart, Date deliveryDateEnd, ProductStatus status) {
+        logger.debug("ProductServiceImpl.fetchAllProductsByCriteria starts ");
+        Object[] params = new Object[] {imei, producer, model, color, storeId, deliveryDateStart, deliveryDateEnd, status};
+        logger.debug("params : [ imei : {} , producer : {} , model : {} , color : {} , storeId : {} , deliveryDateStart : {} , deliveryDateEnd : {}, productStatus : {} ] ", params);
+
+        List<Product> result = productsDao.findByCriteria(imei, producer, model, color, storeId, deliveryDateStart, deliveryDateEnd, status);
+
+        logger.info("ProductServiceImpl.fetchAllProductsByCriteria ends");
+        
+        for(Product p : result) {
+            logger.info(" model : {} , producer : {} ", p.getModel(), p.getProducer() );
+        }
+
+        return  result;
+    }
+
+    public void updateProducts(List<Product> productsToUpdate, List<Product> productsToDelete, List<Product> productsToCancelTheSale, User editor) {
+        logger.debug("ProductServiceImpl.updateProducts starts");
+
+        for (Product p : productsToUpdate) {
+            for (Product p1 : productsToDelete) {
+                if (p1.getId().equals(p.getId())) {
+                    productsToUpdate.remove(p);
+                }
+            }
+            
+            for (Product p2 : productsToCancelTheSale) {
+                if (p2.getId().equals(p.getId())) {
+                    productsToUpdate.remove(p);
+                }
+            }
+
+        }
+
+        if (productsToUpdate.size() > 0) {
+            productsDao.saveOrUpdate(productsToUpdate);
+        }
+
+        if (productsToDelete.size() > 0) {
+            productsDao.markAsDeleted(productsToDelete, editor);
+        }
+
+        if (productsToCancelTheSale.size() > 0) {
+            this.cancelProductsSale(productsToCancelTheSale, editor);
+        }
+    }
+
+    public void cancelProductsSale(List<Product> productsToCancelTheSale, User editor) {
+        logger.debug("ProductServiceImpl.cancelProductsSale starts");
+
+        for (Product p : productsToCancelTheSale) {
+            p.setSale(null);
+            p.setPriceOut(new Money(0,0));
+            p.setEditedAt(new Date());
+            p.setEditor(editor);
+        }
+
+        productsDao.saveOrUpdate(productsToCancelTheSale);
+    }
+
+
+    public void updateProducts(List<Product> products, User updatingUser) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
---
+++ telephony-gwt/src/main/java/telephony/client/gwtp/view/LoginView.java
@@ -0,0 +1,85 @@
+package telephony.client.gwtp.view;
+
+import com.google.gwt.user.client.ui.Widget;
+import com.google.inject.Inject;
+import com.gwtplatform.mvp.client.ViewWithUiHandlers;
+import com.smartgwt.client.types.Alignment;
+import com.smartgwt.client.types.VerticalAlignment;
+import com.smartgwt.client.widgets.IButton;
+import com.smartgwt.client.widgets.events.ClickEvent;
+import com.smartgwt.client.widgets.events.ClickHandler;
+import com.smartgwt.client.widgets.layout.VLayout;
+import telephony.client.gwtp.presenter.LoginPresenter;
+import telephony.client.gwtp.uihandlers.LoginHandlers;
+import telephony.client.ui.widget.form.LoginForm;
+
+
+public class LoginView extends ViewWithUiHandlers<LoginHandlers> implements LoginPresenter.LoginView {
+
+    private LoginForm loginForm;
+    private IButton sendButton;
+
+    private VLayout layout;
+
+
+    @Inject
+    public LoginView() {
+
+        this.layout = new VLayout();
+//        this.layout.setShowEdges(true);
+
+        this.layout.setWidth100();
+        this.layout.setHeight100();
+        this.layout.setLayoutMargin(0);
+
+        this.layout.setMargin(0);
+        this.layout.setPadding(0);
+
+        this.loginForm = new LoginForm();
+        this.loginForm.setWidth100();
+        this.loginForm.setHeight100();
+//        this.loginForm.setShowEdges(true);
+
+        this.loginForm.setLayoutAlign(VerticalAlignment.CENTER);
+        this.loginForm.setLayoutAlign(Alignment.CENTER);
+
+        this.layout.addMember(loginForm);
+
+        this.sendButton = new IButton("Wyślij");
+
+        this.sendButton.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent clickEvent) {
+                getUiHandlers().onOkButtonClicked();
+            }
+        });
+
+        this.layout.addMember(sendButton);
+
+
+    }
+
+
+
+    @Override
+    public Widget asWidget() {
+        return layout;
+    }
+
+    public String getPassword() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public String getLogin() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void resetAndFocus() {
+
+    }
+
+    public void setError(String error) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+
+}
