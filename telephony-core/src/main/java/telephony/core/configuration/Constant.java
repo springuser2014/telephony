52b86119eff0780aa1970d8f2036406a717f3eaa
Paweł Henek <pawelhenek@gmail.com>
2013-03-13 00:23:11 +0100

    Some cleanup in core configuration

--- telephony-core/src/main/java/telephony/core/configuration/Constant.java
+++ telephony-core/src/main/java/telephony/core/configuration/Constant.java
@@ -4,24 +4,14 @@ package telephony.core.configuration;
 /**
  * Some basic costants.
  * @author Paweł Henek <pawelhenek@gmail.com>
- *
  */
 public final class Constant {
-
-    /**
-     *
-     */
+    
     private Constant() {
     }
+    
     /**
-     * asd.
+     * Name of the production persistence unit.
      */
     public static final String PERSISTENCE_UNIT_NAME = "telephony";
-
-    /**
-     * asd.
-     */
-    public static final String APPLICATION_URL_PREFIX = "/adminDashboard/";
-
-    public static final String NONSENS_CONST = "asd";
 }
--- telephony-core/src/main/java/telephony/core/configuration/package-info.java
+++ telephony-core/src/main/java/telephony/core/configuration/package-info.java
@@ -1,5 +1,5 @@
 /**
- * asd.
+ * Contains some basic configuration constants.
  * @author Paweł Henek
  */
 package telephony.core.configuration;
