b627d29252f678f844c190e47df5bf44bc1c1585
Pawel Henek <pawelhenek@gmail.com>
2013-05-21 21:19:06 +0200

    Test data for core module

---
+++ telephony-core/src/test/resources/db/migration/V3__Preparation_to_use_hibernate_envers.sql
@@ -0,0 +1,41 @@
+
+-- remove deleted_at , deleted_by
+
+ALTER TABLE deliveries DROP COLUMN deleted_by;
+ALTER TABLE deliveries DROP COLUMN deleted_at;
+
+
+ALTER TABLE products DROP COLUMN deleted_by;
+ALTER TABLE products  DROP COLUMN deleted_at;
+
+
+ALTER TABLE sales DROP COLUMN deleted_by;
+ALTER TABLE sales DROP COLUMN deleted_at;
+
+
+ALTER TABLE stores DROP COLUMN deleted_by;
+ALTER TABLE stores DROP COLUMN deleted_at;
+
+
+ALTER TABLE roles DROP COLUMN deleted_by;
+ALTER TABLE roles DROP COLUMN deleted_at;
+
+
+ALTER TABLE user_roles DROP COLUMN deleted_by;
+ALTER TABLE user_roles DROP COLUMN deleted_at;
+
+
+ALTER TABLE user_stores DROP COLUMN deleted_by;
+ALTER TABLE user_stores DROP COLUMN deleted_at;
+
+
+ALTER TABLE contacts DROP COLUMN deleted_by;
+ALTER TABLE contacts DROP COLUMN deleted_at;
+
+-- remove system_logs
+
+DROP TABLE system_logs;
+
+-- remove system_log_seq
+
+DROP SEQUENCE system_logs_seq;
