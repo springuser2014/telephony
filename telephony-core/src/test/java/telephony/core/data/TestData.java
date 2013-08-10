0a8a8bb8c382b90b8cfd46db1e9b5e47dd4eef00
Pawel Henek <pawelhenek@gmail.com>
2013-08-10 14:25:10 +0200

    Test data

---
+++ telephony-core/src/test/java/telephony/core/data/TestData.java
@@ -0,0 +1,40 @@
+package telephony.core.data;
+
+import telephony.core.util.StringGeneratorImpl;
+
+/**
+ * asd.
+ * @author pahe
+ */
+public final class TestData {
+	
+	private TestData() {
+		
+	}
+	
+	/**
+	 * asd.
+	 */
+	public static final String USER1_NAME = "user1@gmail.com";
+	public static final String USER1_PASSWORD = "rfaysdhaiufsiuf";	
+	public static final String USER1_SESSIONID = "asdweasdweasdweasdweasdweasdwe21";
+	
+	public static final String USER2_NAME = "user2@gmail.com";
+	public static final String USER2_PASSWORD = "sdaysdhaiufsiua";	
+	
+	public static final String USER3_NAME = "boss@gmail.com";
+	public static final String USER3_PASSWORD = "zwaysdhaiufsiko";	
+	
+	public static final String USER4_NAME = "manager@gmail.com";
+	public static final String USER4_PASSWORD = "wertsdhnbgfsiko";
+	
+	/**
+	 * Generates random sessionId.
+	 * @return Random sessionId.
+	 */
+	public static String randomSessionId() {
+		StringGeneratorImpl g = new StringGeneratorImpl();		
+		return g.nextSessionId();
+	}	
+	
+}
