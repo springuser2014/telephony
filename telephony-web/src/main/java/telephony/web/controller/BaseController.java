96a174b84e54af53b73ee9fab8be363869b4e6f8
Pawel Henek <pawelhenek@gmail.com>
2015-01-27 21:33:21 +0100

    Added views handling in GUI

--- telephony-web/src/main/java/telephony/web/controller/BaseController.java
+++ telephony-web/src/main/java/telephony/web/controller/BaseController.java
@@ -2,6 +2,7 @@ package telephony.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,11 +15,11 @@ import telephony.web.EnvironmentResolver;
 public class BaseController {
 	
 	public BaseController() {
-		
+
 	}
 
 	@RequestMapping(value="/", method = RequestMethod.GET)
-	public ModelAndView main(ModelMap model) {
+	public ModelAndView main() {
 
 		ModelAndView mv = new ModelAndView("dashboard");
 		appendEnvironment(mv);
@@ -26,8 +27,9 @@ public class BaseController {
 		return mv;
 	}
 
+	// deliveries
 	@RequestMapping(value="deliveries", method = RequestMethod.GET)
-	public ModelAndView deliveries(ModelMap model) {
+	public ModelAndView deliveries() {
 
 		ModelAndView mv = new ModelAndView("deliveries");
 		appendEnvironment(mv);
@@ -35,8 +37,27 @@ public class BaseController {
 		return mv;
 	}
 
+	@RequestMapping(value="addDelivery", method = RequestMethod.GET)
+	public ModelAndView addDelivery() {
+
+		ModelAndView mv = new ModelAndView("addDelivery");
+		appendEnvironment(mv);
+
+		return mv;
+	}
+
+	@RequestMapping(value="editDelivery/{id}", method = RequestMethod.GET)
+	public ModelAndView editDelivery(@PathVariable(value="id") Long id) {
+
+		ModelAndView mv = new ModelAndView("editDelivery");
+		appendEnvironment(mv);
+
+		return mv;
+	}
+
+	// products
 	@RequestMapping(value="products", method = RequestMethod.GET)
-	public ModelAndView products(ModelMap model) {
+	public ModelAndView products() {
 
 		ModelAndView mv = new ModelAndView("products");
 		appendEnvironment(mv);
@@ -44,8 +65,18 @@ public class BaseController {
 		return mv;
 	}
 
+	@RequestMapping(value="productDetails/{id}", method = RequestMethod.GET)
+	public ModelAndView productDetails(@PathVariable("id") Long id) {
+
+		ModelAndView mv = new ModelAndView("productDetails");
+		appendEnvironment(mv);
+
+		return mv;
+	}
+
+	// sales
 	@RequestMapping(value="sales", method = RequestMethod.GET)
-	public ModelAndView sales(ModelMap model) {
+	public ModelAndView sales() {
 
 		ModelAndView mv = new ModelAndView("sales");
 		appendEnvironment(mv);
@@ -53,8 +84,36 @@ public class BaseController {
 		return mv;
 	}
 
+	@RequestMapping(value="addSale", method = RequestMethod.GET)
+	public ModelAndView addSale() {
+
+		ModelAndView mv = new ModelAndView("addSale");
+		appendEnvironment(mv);
+
+		return mv;
+	}
+
+	@RequestMapping(value="editSale/{id}", method = RequestMethod.GET)
+	public ModelAndView editSale(@PathVariable("id") Long id) {
+
+		ModelAndView mv = new ModelAndView("editSale");
+		appendEnvironment(mv);
+
+		return mv;
+	}
+
+	@RequestMapping(value="saleDetails/{id}", method = RequestMethod.GET)
+	public ModelAndView saleDetails(@PathVariable("id") Long id) {
+
+		ModelAndView mv = new ModelAndView("saleDetails");
+		appendEnvironment(mv);
+
+		return mv;
+	}
+
+	// contacts
 	@RequestMapping(value="contacts", method = RequestMethod.GET)
-	public ModelAndView contacts(ModelMap model) {
+	public ModelAndView contacts() {
 
 		ModelAndView mv = new ModelAndView("contacts");
 		appendEnvironment(mv);
@@ -62,8 +121,36 @@ public class BaseController {
 		return mv;
 	}
 
+	@RequestMapping(value="addContact", method = RequestMethod.GET)
+	public ModelAndView addContact() {
+
+		ModelAndView mv = new ModelAndView("adContact");
+		appendEnvironment(mv);
+
+		return mv;
+	}
+
+	@RequestMapping(value="editContact/{id}", method = RequestMethod.GET)
+	public ModelAndView editContact(@PathVariable("id") Long id) {
+
+		ModelAndView mv = new ModelAndView("editContact");
+		appendEnvironment(mv);
+
+		return mv;
+	}
+
+	@RequestMapping(value="contactDetails/{id}", method = RequestMethod.GET)
+	public ModelAndView contactDetails(@PathVariable("id") Long id) {
+
+		ModelAndView mv = new ModelAndView("contactDetails");
+		appendEnvironment(mv);
+
+		return mv;
+	}
+
+	// stores
 	@RequestMapping(value="stores", method = RequestMethod.GET)
-	public ModelAndView stores(ModelMap model) {
+	public ModelAndView stores() {
 
 		ModelAndView mv = new ModelAndView("stores");
 		appendEnvironment(mv);
@@ -71,8 +158,27 @@ public class BaseController {
 		return mv;
 	}
 
+	@RequestMapping(value="addStore", method = RequestMethod.GET)
+	public ModelAndView addStore() {
+
+		ModelAndView mv = new ModelAndView("addStore");
+		appendEnvironment(mv);
+
+		return mv;
+	}
+
+	@RequestMapping(value="editStore/{id}", method = RequestMethod.GET)
+	public ModelAndView editStore(@PathVariable("id") Long id) {
+
+		ModelAndView mv = new ModelAndView("editStore");
+		appendEnvironment(mv);
+
+		return mv;
+	}
+
+	// taxes
 	@RequestMapping(value="taxes", method = RequestMethod.GET)
-	public ModelAndView taxes(ModelMap model) {
+	public ModelAndView taxes() {
 
 		ModelAndView mv = new ModelAndView("taxes");
 		appendEnvironment(mv);
@@ -80,8 +186,27 @@ public class BaseController {
 		return mv;
 	}
 
+	@RequestMapping(value="addTax", method = RequestMethod.GET)
+	public ModelAndView addTax() {
+
+		ModelAndView mv = new ModelAndView("addTax");
+		appendEnvironment(mv);
+
+		return mv;
+	}
+
+	@RequestMapping(value="editTax/{id}", method = RequestMethod.GET)
+	public ModelAndView editTax(@PathVariable("id") Long id) {
+
+		ModelAndView mv = new ModelAndView("editTax");
+		appendEnvironment(mv);
+
+		return mv;
+	}
+
+	// complaints
 	@RequestMapping(value="complaints", method = RequestMethod.GET)
-	public ModelAndView complaints(ModelMap model) {
+	public ModelAndView complaints() {
 
 		ModelAndView mv = new ModelAndView("oomplaints");
 		appendEnvironment(mv);
@@ -89,8 +214,45 @@ public class BaseController {
 		return mv;
 	}
 
+	@RequestMapping(value="addComplaint", method = RequestMethod.GET)
+	public ModelAndView addComplaint() {
+
+		ModelAndView mv = new ModelAndView("addComplaint");
+		appendEnvironment(mv);
+
+		return mv;
+	}
+
+	@RequestMapping(value="editComplaint/{id}", method = RequestMethod.GET)
+	public ModelAndView editComplaint(@PathVariable("id") Long id) {
+
+		ModelAndView mv = new ModelAndView("editComplaint");
+		appendEnvironment(mv);
+
+		return mv;
+	}
+
+	@RequestMapping(value="complaintDetails/{id}", method = RequestMethod.GET)
+	public ModelAndView complaintDetails(@PathVariable("id") Long id) {
+
+		ModelAndView mv = new ModelAndView("complaintDetails");
+		appendEnvironment(mv);
+
+		return mv;
+	}
+
+	@RequestMapping(value="commentComplaintAnonymously/{id}", method = RequestMethod.GET)
+	public ModelAndView commentComplaintAnonymously(@PathVariable("id") Long id) {
+
+		ModelAndView mv = new ModelAndView("commentComplaintAnonymously");
+		appendEnvironment(mv);
+
+		return mv;
+	}
+
+	// users
 	@RequestMapping(value="users", method = RequestMethod.GET)
-	public ModelAndView users(ModelMap model) {
+	public ModelAndView users() {
 
 		ModelAndView mv = new ModelAndView("users");
 		appendEnvironment(mv);
@@ -98,8 +260,27 @@ public class BaseController {
 		return mv;
 	}
 
+	@RequestMapping(value="addUser", method = RequestMethod.GET)
+	public ModelAndView addUser() {
+
+		ModelAndView mv = new ModelAndView("addUser");
+		appendEnvironment(mv);
+
+		return mv;
+	}
+
+	@RequestMapping(value="editUser/{id}", method = RequestMethod.GET)
+	public ModelAndView editUser(@PathVariable("id") Long id) {
+
+		ModelAndView mv = new ModelAndView("editUser");
+		appendEnvironment(mv);
+
+		return mv;
+	}
+
+	// misc
 	@RequestMapping(value="login", method = RequestMethod.GET)
-	public ModelAndView login(ModelMap model) {
+	public ModelAndView login() {
 
 		ModelAndView mv = new ModelAndView("login");
 		appendEnvironment(mv);
@@ -108,7 +289,7 @@ public class BaseController {
 	}
 
 	@RequestMapping(value="logout", method = RequestMethod.GET)
-	public ModelAndView logout(ModelMap model) {
+	public ModelAndView logout() {
 
 		ModelAndView mv = new ModelAndView("logout");
 		appendEnvironment(mv);
